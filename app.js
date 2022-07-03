4404e71a69fbf5c6d35b7081b00ac1d026774405
32-Mimhakkani <mim.cse32@gmail.com>
2022-07-03 18:07:27 +0600

    block and global scope

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ var a = 10;
 let aa = 20;
 const pp = 30
 
-console.log(a,aa,pp);
+// console.log(a,aa,pp);
+
+// global scope 
+
+if(aa===20){
+ console.log('global scope var::',a);
+ let name = 'mim';
+ console.log('block scope var::',name);
+}
+
+//  console.log('block scope var 2 check::',name); it is not working for block scope
