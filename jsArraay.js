c79090d9c9d2dae7c8c9bb855b9f39bf74743ee3
32-Mimhakkani <mim.cse32@gmail.com>
2022-10-02 18:46:30 +0600

    array fill

--- jsArraay.js
+++ jsArraay.js
@@ -5,13 +5,24 @@
 const number= [1,5,15,25];
 // console.log(number);  // a[1,5,15,25]
 
+
+
 //   push the array  (add value in the last index )
 
     number.push(55);
     number.push('mim')
     // console.log(number);  // [ 1, 5, 15, 25, 55, 'mim' ]
 
+
+
 //  pop (delete the last element of the array )
 
-   number.pop();
-    // console.log(number);  // [ 1, 5, 15, 25, 55 ]
+     number.pop();
+    //  console.log(number);  // [ 1, 5, 15, 25, 55 ]
+
+
+
+// fill (fill all elements in same to same )
+
+  //  number.fill('mim');  [ 'mim', 'mim', 'mim', 'mim', 'mim' ]
+    //  console.log(number);
