ffdf3bd7fa04d7cbb99b2a89a1694674f569de18
32-Mimhakkani <mim.cse32@gmail.com>
2022-10-02 18:41:51 +0600

    js declare, push and pop method

---
+++ jsArraay.js
@@ -0,0 +1,17 @@
+// play with javaScript array 
+
+//   1. declare with array 
+
+const number= [1,5,15,25];
+// console.log(number);  // a[1,5,15,25]
+
+//   push the array  (add value in the last index )
+
+    number.push(55);
+    number.push('mim')
+    // console.log(number);  // [ 1, 5, 15, 25, 55, 'mim' ]
+
+//  pop (delete the last element of the array )
+
+   number.pop();
+    // console.log(number);  // [ 1, 5, 15, 25, 55 ]
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+// play with javaScript array 
+
+//   1. declare with array 
+
+const number= [1,5,15,25];
+// console.log(number);  // a[1,5,15,25]
+
+//   push the array 
+
+number.push(55);
+number.push('mim')
+
+console.log(number);
