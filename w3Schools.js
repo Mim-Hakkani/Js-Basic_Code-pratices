da93215e761ee29627b54a9062ebc603db8e2237
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-10-12 17:02:55 +0600

    js font size

--- w3Schools.js
+++ w3Schools.js
@@ -1,4 +1,7 @@
+/*************
+     Power Of JavaScript Code 
 
+  ************** */
 // inner html 
 
 document.getElementById('seeelements').innerHTML=`<div>
@@ -12,5 +15,13 @@ document.getElementById('seeelements').innerHTML=`<div>
 
 document.getElementById("changeText").innerText=`Ehsan Software in the Firm`
 
+
+// change attributes 
+
+ document.getElementById("changewidth").src="m2.png"
+
+ // change style 
+ document.getElementById("changeStyles").style.fontSize="52px"
+ document.getElementById("changeStyles").style.color="violet"
 
 
