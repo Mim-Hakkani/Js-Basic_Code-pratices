32ccd5c371b0eb2a7581b19e1f08cb55c9657a60
Mim-Hakkani <mimhakkani4g@gmail.com>
2022-10-12 16:38:59 +0600

    inner text

--- w3Schools.js
+++ w3Schools.js
@@ -1,3 +1,16 @@
-document.getElementById('seeelements').innerHTML=`
+
+// inner html 
+
+document.getElementById('seeelements').innerHTML=`<div>
 <h1 style="color:red">This is the Header </h1>
-<p>Golam Hakkani Mim</p>`;
+<p>Golam Hakkani Mim</p>
+<button>Hakkani </button>
+</div> `;
+
+
+// inner text 
+
+document.getElementById("changeText").innerText=`Ehsan Software in the Firm`
+
+
+
