8ac612be2798ee831b2c9cda02d4116f54c9c9f2
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-09 18:06:52 +0600

    print the console output in code edittor

---
+++ Junayed/Day1/day1.js
@@ -0,0 +1,10 @@
+/* How to Setup Js 
+
+step 1 : Google Crome console 
+step 2 : in command file 
+step 3 : using html file to link up .js
+step 4 : code edittor  
+
+*/
+
+console.log('Wellcome To The Js World !! ');
