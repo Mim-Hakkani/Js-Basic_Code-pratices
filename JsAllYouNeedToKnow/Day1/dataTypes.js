d10334895d17aee4a1320df2980f23da563c8ee6
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-12 15:47:52 +0600

    data types in premitives with examples

---
+++ JsAllYouNeedToKnow/Day1/dataTypes.js
@@ -0,0 +1,41 @@
+// premitive 
+
+  // Number 
+
+   let num1 = 5;  // int 
+   let num2 = 5.6  // float 
+
+  // String
+
+    let name = "Golam Hakkani mim "  // using double quotes 
+    let companyName = 'Ehsan Marketing '  // using single quotes 
+    let jobPost = `React Js Developer `   // using backtic 
+
+  // Boolean 
+
+   let isTrue = true 
+   let isFlase = false 
+
+  // null 
+
+   let empty = null  // assign but null not undefined 
+
+  // undefined 
+
+   let x;
+   console.log(x); // output undefined 
+
+  // symbol 
+
+let sym1 = Symbol();
+let sym2 = Symbol("mySymbol");
+
+console.log(Symbol("mySymbol"));
+
+// nonPremitive 
+
+// object 
+   
+    // Array 
+    // Function 
+    // Object 
