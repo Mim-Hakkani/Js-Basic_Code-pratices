076a60ae1d9950469a1a33804181cbd2775b3777
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-12 15:30:57 +0600

    js reverved keyword and nemming convension

---
+++ JsAllYouNeedToKnow/Day1/revervedKeyword.js
@@ -0,0 +1,8 @@
+// const false = "mmm";
+// const else = 10;
+
+// its not working because it is the revered worrd that i am not used
+
+
+// always use camel case when using js code 
+const ehsanMarketingEmployee="Mim Hakakni"
