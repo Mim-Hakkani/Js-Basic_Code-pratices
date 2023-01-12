89519c83a53f9aa34b4ab7283e1713b853415a45
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-12 15:26:19 +0600

    variables is done

---
+++ JsAllYouNeedToKnow/Day1/variables.js
@@ -0,0 +1,11 @@
+
+const name ="Golam";
+const name2 = "Hakkani mim"
+const age = 28;
+const sum = 10;
+let sum2 = 5+105.4
+console.log("Name :",name+' '+name2);
+console.log('My age is : ',age);
+console.log("summation : ",sum);
+console.log("Summation 2 is : ", sum2);
+
