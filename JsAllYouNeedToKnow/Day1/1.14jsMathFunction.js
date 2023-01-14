c6007e5f89985e3c118f530648d8316074a9c5f4
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-14 16:29:15 +0600

    js math function

---
+++ JsAllYouNeedToKnow/Day1/1.14jsMathFunction.js
@@ -0,0 +1,13 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+const n = 4.325;
+console.log('Absulate :',Math.abs(n));
+console.log('floor :',Math.floor(n));
+console.log('Ceil :',Math.ceil(n));
+console.log('Round :',Math.round(n));
+console.log('Maximum number :',Math.max(122,355,488,5));
+console.log('Minimum number is :',Math.min(122,355,488,5));
+console.log('Sqrt is :',Math.sqrt(64));
+console.log('Power is :',Math.pow(2,3));
+console.log('Random is :',Math.round(Math.random()*50));
