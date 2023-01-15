2962c106b449be708a4909362da1c8f14fe7bebc
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-15 20:07:45 +0600

    composition function

---
+++ JsAllYouNeedToKnow/DAy9/9-9FunctionComposition.js
@@ -0,0 +1,15 @@
+/*
+
+ekta function er input jodi onno arek ta function er output hoi then take function composion bole 
+
+*/
+
+function print(inp){
+ console.log(inp);
+}
+
+function add(a,b){
+    return a+b;
+}
+
+print(add(3,4))
