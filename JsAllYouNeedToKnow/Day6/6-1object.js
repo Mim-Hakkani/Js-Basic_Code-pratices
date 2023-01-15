60ad11578f4dd98e88e5e01529376bddeba0dd59
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-15 11:15:51 +0600

    object create ,add ,show value

---
+++ JsAllYouNeedToKnow/Day6/6-1object.js
@@ -0,0 +1,21 @@
+const std = {
+    stdName : "Mim",
+    Roll : 150132,
+    dept:'cse'
+}
+
+// add property in object 
+
+   std.university = "pust"
+
+// console.log(std);
+
+
+// see the object value using . dot notation 
+
+// console.log(std.Roll);
+
+
+// see the object value using array notation 
+
+ console.log(std['Roll']);
