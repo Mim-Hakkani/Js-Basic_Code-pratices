850d7a8ec1ac7d037aa2b404dde38844568ccc24
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-15 12:12:43 +0600

    fuction call and function definition is done

---
+++ JsAllYouNeedToKnow/Day7/7-1functiondefination.js
@@ -0,0 +1,33 @@
+function add(){
+  let a = 5;
+  let b = 10;
+  console.log(a+b);
+}
+
+
+function sub(){
+  let a = 5;
+  let b = 10;
+  console.log(a-b);
+}
+
+function mul(){
+  let a = 5;
+  let b = 10;
+  console.log(a*b);
+}
+
+function div(){
+  let a = 5;
+  let b = 10;
+  console.log(a/b);
+}
+
+function arithmeticOperaiton(){
+    add(); // addition function call 
+    sub(); // Substraction function call 
+    mul(); // multiplication function call 
+    div(); //divition function call 
+}
+
+arithmeticOperaiton()
