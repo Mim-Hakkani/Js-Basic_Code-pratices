b87804a7c8f370c807d9506ce087f7759d50ec53
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-15 18:51:30 +0600

    filter

--- JsAllYouNeedToKnow/DAy9/9-2Map.js
+++ JsAllYouNeedToKnow/DAy9/9-2Map.js
@@ -10,7 +10,6 @@
 // console.log(brotherNames);
 // console.log(arr);
 
-
 /******************** For Each ***************** */
 
 // let arr = ['mim','jim','rabbanib','rafia']
@@ -23,8 +22,6 @@
 // console.log(brotherNames);
 // console.log(arr);
 
-
-
 /************** Different Between Maps and forEach ******************* */
 
 // map is a method that creates a new array with the results of calling a provided function on every element in the calling array. It does not modify the original array.
@@ -33,11 +30,9 @@
 // let doubledNumbers = numbers.map(x => x * 2);
 // console.log(doubledNumbers); // [2, 4, 6]
 
-
 // forEach is a method that executes a provided function once for each array element. It does not create a new array and does not return anything.
 // let numbers = [1, 2, 3];
-// numbers.forEach(x => console.log(x * 2)); 
-// Output: 2 4 6 
-
+// numbers.forEach(x => console.log(x * 2));
+// Output: 2 4 6
 
 // So in short, map returns a new array after applying the provided function on each element, while forEach applies the provided function on each element but doesn't return anything.
---
+++ JsAllYouNeedToKnow/DAy9/9-3Filter.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3,4,5,6,7,8,91,15,14,18]
+
+let newArray = arr.filter(function(value){
+   return (value%2==0) // if even number 
+//    return (value%2==1) // if odd number 
+
+
+   
+})
+
+console.log(newArray);
