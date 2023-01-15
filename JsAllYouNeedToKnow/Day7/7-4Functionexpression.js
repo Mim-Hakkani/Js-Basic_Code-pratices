1ac49f07754443f59a2369091e43fea20bc6198a
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-15 14:43:33 +0600

    function expression

---
+++ JsAllYouNeedToKnow/Day7/7-4Functionexpression.js
@@ -0,0 +1,32 @@
+const value = function(a,b){
+    sum = a+b;
+    return sum
+}
+
+console.log(value(2,3));
+
+
+
+// use it function as expression  meand used function as variables ...
+ 
+ if(value(2,3)>5){
+    console.log(true);
+ }
+ else{
+    console.log(false);
+ }
+
+
+// es6 
+
+    // const handlechick = ()=>{
+
+    // }
+
+
+    // asycrouns function 
+
+    setTimeout(function(){
+        console.log('ami kiso buji nai ');
+        console.log(value(6,5))
+    },5000)
