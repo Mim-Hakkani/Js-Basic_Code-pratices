b0ee921c6db08b207f6391e8f1a73edc3d276990
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-15 13:01:56 +0600

    function return value boolean array aand objects

---
+++ JsAllYouNeedToKnow/Day7/7-5Returnvalue.js
@@ -0,0 +1,31 @@
+
+//return value 
+function sum(a,b){
+    return a+b;
+}
+let total = sum(5,10)
+// console.log("Total :: ", total);
+
+
+// return object 
+
+function person(name,roll){
+    return{
+        name,
+        roll
+    }
+}
+
+const std = person('mim',150132);
+// console.log(std);
+
+
+
+ // return array 
+
+ function letters(first,second,third){
+  return[first,second,third]
+ }
+let letsAll = letters('a','b','c')
+
+// console.log(letsAll);
