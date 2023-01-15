b7f82b32c8dde8f8354bc11300b9e53a60a763e1
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-15 10:48:43 +0600

    array join method

---
+++ JsAllYouNeedToKnow/Day4/4-4ArrayMethod.js
@@ -0,0 +1,28 @@
+// cancat two array 
+
+let arr = [1,2,3]
+let arr2 = [4,5,6]
+
+let arr3 = arr.concat(arr2)
+// console.log(arr3);
+
+
+// create array duplicate form 
+
+ let arr4 = Array.from(arr);
+ arr4[2] = 10;
+//  console.log(arr4);
+//  console.log(arr);
+
+
+// isArray method to check that it is array or not 
+
+// console.log(Array.isArray(arr4));
+
+
+// join method 
+
+let arrayJoin = arr4.join(", ");
+// console.log(arrayJoin);
+// console.log(arrayJoin[2]);
+
