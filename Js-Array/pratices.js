8dfd5728e92b8ad7e10db24c9de0cd7b05db28b2
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-07-25 14:17:58 +0600

    js arry

---
+++ Js-Array/pratices.js
@@ -0,0 +1,51 @@
+// concat(): Merges two or more arrays into a new array.
+
+const letter =['a','b','c'];
+const  digit = [0,1,2];
+
+const result = letter.concat(digit);
+console.log("concat result : ", result);
+
+// copyWithin(): Copies elements within an array, to and from specified positions.
+
+// entries(): Returns a key/value pair Array Iteration Object.
+
+// every(): Checks if every element in an array pass a test.
+
+// fill(): Fills an array with a specific value.
+
+// filter(): Creates a new array with elements that pass a test.
+
+// find(): Returns the first element in an array that passes a test.
+
+// findIndex(): Returns the index of the first element in an array that passes a test.
+
+// forEach(): Executes a function for each element in an array.
+
+// includes(): Returns true if an array contains a specified value.
+
+// indexOf(): Searches the array for an element and returns its position.
+
+// lastIndexOf(): Searches the array for an element and returns its position, starting from the end.
+
+// map(): Creates a new array with the results of calling a function on each element in the original array.
+
+// reduce(): Reduces an array to a single value by applying a function to each element and accumulating the results.
+
+// reduceRight(): Reduces an array to a single value by applying a function to each element in reverse order and accumulating the results.
+
+// reverse(): Reverses the order of the elements in an array.
+
+// slice(): Returns a new array that contains a section of the original array.
+
+// some(): Checks if at least one element in an array passes a test.
+
+// sort(): Sorts the elements in an array.
+
+// splice(): Removes and/or replaces elements in an array.
+
+// toLocaleString(): Converts an array to a string in a locale-specific way.
+
+// toString(): Converts an array to a string.
+
+// unshift(): Adds one or more elements to the beginning of an array.
