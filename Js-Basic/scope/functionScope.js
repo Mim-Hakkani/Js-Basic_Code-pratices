fba9d4b2be78874e44a72d64fb2bb0db036b3483
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-12-20 22:34:11 +0600

    function scope understand

---
+++ Js-Basic/scope/functionScope.js
@@ -0,0 +1,10 @@
+var name = "Bangla"
+
+function changeName(data){
+    var name = data;
+    console.log(`I love ${name}`);
+}
+
+// functrion call 
+changeName("English");
+console.log("I know",name);
